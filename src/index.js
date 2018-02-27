5bb734cb11b835fa5e4d4ebdf8a052437b286731
Ricardo Gosch <ricardogosch@gmail.com>
2018-02-27 00:44:23 -0300

    Personalize listagemHoteis

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import './sass/main.sass';
 
 let component = () => {
+    // Remove elements
+    (() => {
+        // Remove "Quero este quarto"
+        $('.itemBtnSelecionarAcomodacao').remove()
 
+        // Remove percentual
+        $('.itemVarPercentualDesconto').remove()
+
+        // Remove parse
+        $('.itemVarDescricaoFormasPagamento').remove()
+
+    })()
 }
+component()
