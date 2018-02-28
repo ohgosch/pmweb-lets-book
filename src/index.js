2a2aa1a843461276a69d9e1588d70e4fb62da5c1
Ricardo Gosch <ricardogosch@gmail.com>
2018-02-28 00:07:29 -0300

    Fix slider and final colors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './sass/main.sass';
+import './sass/main.sass'
 
 let component = () => {
     /**
@@ -26,9 +26,24 @@ let component = () => {
     $('#navContent > a').html('<img src="//i.imgur.com/FGgG2NO.png" alt="Logotipo Pmweb" />')
 
     // Logo Header href
-    $('#navContent > a').attr('href', 'https://www.pmweb.com.br/').attr('title', 'Pmweb').attr('target', '_blank');
+    $('#navContent > a').attr('href', 'https://www.pmweb.com.br/').attr('title', 'Pmweb').attr('target', '_blank')
 
     // Logo Footer
-    $('#footerContent .footerFirstLine p:nth-child(2)').html('<a href="" title="Desenvolvido por Pmweb"><img src="//i.imgur.com/FGgG2NO.png" alt="Logotipo Pmweb" /><a/>')
+    $('#footerContent .footerFirstLine p:nth-child(2)').html('<a href="https://www.pmweb.com.br/" target="_blank" title="Desenvolvido por Pmweb"><img src="//i.imgur.com/FGgG2NO.png" alt="Logotipo Pmweb" /><a/>')
+
+    // Slider lazy auto update
+    let currentSlide = $('.slick-list').find('.slick-slide.slick-current.slick-active')
+    // let slides = $('.slick-list .slick-slide')
+    let width = $('.slider-imagens-hotel')[0].clientWidth
+    currentSlide.each((index, element) => {
+        let el = $(element)
+        let img = el.find('img')
+        let src = el.find('a').attr('data-thumb')
+        el.css({width: width})
+        img.src = src
+
+    })
+    $('.slider-imagens-hotel').slick('slickNext')
+    $('.slider-imagens-hotel').slick('slickPrev')
 }
 component()
